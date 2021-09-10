2c057b0502528576d004b03239ad98666e8c4147
soundharvs <soundharvs2001@gmail.com>
2021-09-10 20:04:09 +0530

    Added loop statements
    Signed-off-by: soundharvs <soundharvs2001@gmail.com>

--- hello.rb
+++ hello.rb
@@ -1,9 +1,31 @@
+#hello
 puts"Hello World"
+
+#getting inputs
 puts"whats your name my boi?"
-
 name = gets
-
 puts "name:"+name+" noice name "
 
+#ternary condition
 a=gets.chomp.to_i
 puts(a>=18?"can vote" : "cannot vote")
+
+#forloop
+for i in 1..5
+    puts i
+end
+
+#whileloop
+j=gets.chomp.to_i
+while(j>0)
+    puts j
+    j=j-1
+    break if j==0
+end
+
+#until loop
+i=1
+until i==10
+    puts i*10,"\n"
+    i+=1
+end
